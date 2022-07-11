6fcd0fa2e681e8669a6ccea3eac657c7212e0765
martingrondein-nls <martin@nlsltd.com>
2022-07-11 15:55:48 +0100

    deployment changes 20

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+# cd into the active working directory on the ec2 instance
+cd /var/www
+# bring down the running images
+sudo docker-compose down
+# check for and then stop the docker service
 isExistApp=pgrep docker
 if [[ -n $isExistApp ]]; then
 sudo service docker stop
 fi
+# forcefully Remove Containers and Images
+sudo docker rm -f $(docker ps -qa)
+yes | sudo docker image prune -a
