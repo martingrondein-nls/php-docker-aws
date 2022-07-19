28db882d6e277b15208e25a25f84314d2a6468b5
martingrondein-nls <martin@nlsltd.com>
2022-07-19 14:43:44 +0100

    deployment changes 80

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -23,6 +23,7 @@ then
 
     echo "export ECR_REGISTRY=$ECR_REGISTRY" | sudo tee /etc/profile.d/aws_kms.sh
     echo "export ECR_VER=$ECR_VER" | sudo tee  -a /etc/profile.d/aws_kms.sh
+    echo "export APPLICATION_NAME=$APPLICATION_NAME" | sudo tee  -a /etc/profile.d/aws_kms.sh
 
     source /etc/profile.d/aws_kms.sh
 fi
